ae5dfb7cc98c0019f6011fb7afce42f2336213f4
Aneesh Garg <garganeeshk@gmail.com>
2024-10-09 18:22:31 -0700

    completed team tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -11,16 +11,61 @@ public class TeamTest {
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("f24-10");    
     }
 
     @Test
     public void getName_returns_correct_name() {
-       assert(team.getName().equals("test-team"));
+       assert(team.getName().equals("f24-10"));
     }
 
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        team.setName("f24-10");
+        team.addMember("Ajay");
+        team.addMember("Aneesh");
+        team.addMember("Derrick");
+        team.addMember("Peter");
+        team.addMember("Rajvir");
+        team.addMember("Justin");
+        assertEquals("Team(name=f24-10, members=[Ajay, Aneesh, Derrick, Peter, Rajvir, Justin])", team.toString());
+    }
 
+    @Test
+    public void equals_returns_true_for_same_team() {
+        // Same object (pointers two both objects are same)
+        assertEquals(team.equals(team), true);
+
+        // Other object isn't instance of this class
+        assertEquals(team.equals(1), false);
+
+        // Compare fields
+        // TRUE TRUE case
+        Team other = new Team("f24-10");
+        Team twin = new Team("f24-10");
+        assertEquals(true, twin.equals(other));
+
+        // FALSE TRUE
+        Team other2 = new Team("f24-10");
+        other2.addMember("teehee");
+        assertEquals(team.equals(other2), false);
+
+        // TRUE FALSE
+        Team other3 = new Team("wrong name");
+        other3.addMember("Ajay");
+        other3.addMember("Aneesh");
+        other3.addMember("Derrick");
+        other3.addMember("Peter");
+        other3.addMember("Rajvir");
+        other3.addMember("Justin");
+        assertEquals(team.equals(other3), false);
+    }
+
+    @Test
+    public void hashCode_check_hashCode_value() {
+        assertEquals(team.hashCode(), -1327023899);
+    }
 }
